f04c4d9e04215f6a7e89ba42f00d85a30bce6caa
serzh251 <serzh251@mail.ru>
2021-08-03 14:30:18 +0300

    merge

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken import views
 from usersapp.views import UserViewSet
-from usersapp import views
-# from todo import views
 from todo.views import ProjectViewSet, ToDoViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = DefaultRouter()
-# router.register('users', UserViewSet)
-router.register('users', views.UserViewSet, basename='users')
+router.register('users', UserViewSet)
+# router.register('users', views.UserViewSet, basename='users')
 router.register('project', ProjectViewSet, basename='project')
 router.register('todo', ToDoViewSet, basename='todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
